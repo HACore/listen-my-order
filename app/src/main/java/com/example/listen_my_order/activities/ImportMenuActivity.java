0459b717fcbbbfdda262832b9b0b14a3b1a82b9b
HACore <lhe5254@gmail.com>
2021-08-27 13:38:37 +0900

    Add actionbar back button

--- app/src/main/java/com/example/listen_my_order/activities/ImportMenuActivity.java
+++ app/src/main/java/com/example/listen_my_order/activities/ImportMenuActivity.java
@@ -1,10 +1,14 @@
 package com.example.listen_my_order.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.listen_my_order.R;
@@ -17,6 +21,9 @@ import euphony.lib.receiver.EuRxManager;
 
 public class ImportMenuActivity extends AppCompatActivity {
 
+    private ActionBar appbar;
+    private boolean mode = true;
+    private Button setImportButton;
     private TextView storeNameView;
     private RecyclerView menuListView;
 
@@ -27,9 +34,30 @@ public class ImportMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_menu);
 
+        appbar = getSupportActionBar();
+        appbar.setTitle("Menu list");
+        appbar.setDisplayHomeAsUpEnabled(true);
+
+        setImportButton = (Button) findViewById(R.id.btn_set_import);
         storeNameView = (TextView) findViewById(R.id.tv_store_name);
         menuListView = (RecyclerView) findViewById(R.id.rv_menu_list);
         menuListView.setLayoutManager(new LinearLayoutManager(this));
+
+        setImportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mode) {
+                    mRxManager.finish();
+                    setImportButton.setText(R.string.btn_start_listen);
+                    mode = false;
+                }
+                else {
+                    mRxManager.listen();
+                    setImportButton.setText(R.string.btn_stop_listen);
+                    mode = true;
+                }
+            }
+        });
 
         mRxManager.setAcousticSensor(new AcousticSensor() {
             @Override
@@ -44,5 +72,16 @@ public class ImportMenuActivity extends AppCompatActivity {
             }
         });
         mRxManager.listen();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
